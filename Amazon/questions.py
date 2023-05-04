a9a3511ecc4ba4f276945736d7d31b651951c53a
kodebae <heykarmendurbin@gmail.com>
2023-05-04 00:59:01 -0400

    chore: missing number

--- Amazon/questions.py
+++ Amazon/questions.py
@@ -2,6 +2,8 @@
 # Amazon Question Bank
 
 # 1. Contains Duplicate ʕ´•ᴥ•`ʔ
+# Given an integer array nums, return true if any value appears at least twice in the array, 
+# and return false if every element is distinct.
 
 def containsDuplicate(nums):
         hashset = set()
@@ -18,7 +20,25 @@ main()
 
 # Time: O(N) 
 # Space: 0(N)
-
 #### ᕕ༼✿•̀︿•́༽ᕗ ####
 
-# 2. 
+
+
+# 2. Missing Number ʕ´•ᴥ•`ʔ
+# Given an array nums containing n distinct numbers in the range [0, n], 
+# return the only number in the range that is missing from the array.
+
+def missingNumber(nums):
+        res = len(nums)
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
+
+def main():
+    print(missingNumber([3,0,1]))
+main()
+
+
+# Time: O(N) 
+# Space: 0(1)
+#### ᕕ༼✿•̀︿•́༽ᕗ ####
